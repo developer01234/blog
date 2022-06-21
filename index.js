9d92f2f64011d82d9074041ff60ce6d71bf7392a
Ruslan <ruslansorokin234567@gmail.com>
2022-06-21 15:39:07 +0300

    UserShema

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+import { registerValidator } from './validations/auth.js'
+import { validationResult } from 'express-validator'
+
+mongoose
+	.connect(
+		'mongodb+srv://blog:Test123@cluster0.ji6m8.mongodb.net/?retryWrites=true&w=majority'
+	)
+	.then(() => console.log('DB OK!'))
+	.catch(err => console.log('DB ERROR!', err))
+
 const app = express()
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+app.use(express.json())
+
+app.post('/auth/register', registerValidator, (req, res) => {
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		return res.status(400).json(errors.array())
+	}
+
+	res.json({
+		success: true,
+	})
 })
 
 app.listen(5000, err => {
-	if (err) return console.log('Server Syka Blyat', err)
+	if (err) {
+		return console.log('Server Kill!', err)
+	}
 
 	console.log('Server OK!')
 })
