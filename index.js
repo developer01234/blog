143b4bb53942cf27c91504b1839c694c17ea92b4
Ruslan <ruslansorokin234567@gmail.com>
2022-06-21 18:14:17 +0300

    editorconfig

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ import { registerValidator } from './validations/auth.js'
 import { validationResult } from 'express-validator'
 
 mongoose
-	.connect(
-		'mongodb+srv://blog:Test123@cluster0.ji6m8.mongodb.net/?retryWrites=true&w=majority'
-	)
-	.then(() => console.log('DB OK!'))
-	.catch(err => console.log('DB ERROR!', err))
+  .connect(
+    'mongodb+srv://blog:Test123@cluster0.ji6m8.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('DB OK!'))
+  .catch((err) => console.log('DB ERROR!', err))
 
 const app = express()
 
 app.use(express.json())
 
 app.post('/auth/register', registerValidator, (req, res) => {
-	const errors = validationResult(req)
-	if (!errors.isEmpty()) {
-		return res.status(400).json(errors.array())
-	}
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array())
+  }
 
-	res.json({
-		success: true,
-	})
+  res.json({
+    success: true
+  })
 })
 
-app.listen(5000, err => {
-	if (err) {
-		return console.log('Server Kill!', err)
-	}
+app.listen(5000, (err) => {
+  if (err) {
+    return console.log('Server Kill!', err)
+  }
 
-	console.log('Server OK!')
+  console.log('Server OK!')
 })
--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,8 @@
 import { body } from 'express-validator'
 
 export const registerValidator = [
-	body('email').isEmail(),
-	body('password').isLength({ min: 5 }),
-	body('fullName').isLength({ min: 3 }),
-	body('avatarUrl').optional().isURL(),
+  body('email').isEmail(),
+  body('password').isLength({ min: 5 }),
+  body('fullName').isLength({ min: 3 }),
+  body('avatarUrl').optional().isURL()
 ]
