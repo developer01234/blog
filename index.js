f1a4ada4091a9c025602d749d7b24118e7cba1d8
Ruslan <ruslansorokin234567@gmail.com>
2022-06-26 21:16:47 +0300

    login

--- index.js
+++ index.js
@@ -17,6 +17,38 @@ const app = express();
 
 app.use(express.json());
 
+app.post("/auth/login", async (req, res) => {
+	try {
+		const user = await UserModel.findOne({ email: req.body.email });
+
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+
+		const isValidPass = await bcrypt.compare(
+			req.body.password,
+			user._doc.passwordHash
+		);
+
+		if (!isValidPass) {
+			return res.status(400).json({
+				message: "Invalid login or password",
+			});
+		}
+
+		const token = jwt.sign({ _id: user._id }, "top-secret", {
+			expiresIn: "30d",
+		});
+
+		const { passwordHash, ...userData } = user._doc;
+
+		res.json({ ...userData, token });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: "Failed to login" });
+	}
+});
+
 app.post("/auth/register", registerValidator, async (req, res) => {
 	try {
 		const errors = validationResult(req);
@@ -26,20 +58,27 @@ app.post("/auth/register", registerValidator, async (req, res) => {
 
 		const password = req.body.password;
 		const salt = await bcrypt.genSalt(10);
-		const passwordHash = await bcrypt.hash(password, salt);
+		const hash = await bcrypt.hash(password, salt);
 
 		const doc = new UserModel({
 			email: req.body.email,
 			fullName: req.body.fullName,
 			avatarUrl: req.body.avatarUrl,
-			passwordHash,
+			passwordHash: hash,
 		});
 
 		const user = await doc.save();
 
-		res.json(user);
+		const token = jwt.sign({ _id: user._id }, "top-secret", {
+			expiresIn: "30d",
+		});
+
+		const { passwordHash, ...userData } = user._doc;
+
+		res.json({ ...userData, token });
 	} catch (err) {
-		res.status(500).json({ message: "Invalid password or email" });
+		console.log(err);
+		res.status(500).json({ message: "Failed registration" });
 	}
 });
 
